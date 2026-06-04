784eda628f05de888c193ba763557f48fc1bd3a1
amandagm77 <amanda.gmb@outlook.com>
2026-06-03 21:55:52 -0600

    Added Infinite Scroll

--- js/main.js
+++ js/main.js
@@ -10,17 +10,19 @@ const loader = document.getElementById("loader");
 loader.classList.add("hidden");
 const errorMessage = document.getElementById("error-message");
 let offset = 0;
+let currentSearchTerm = "";
+let isLoading = false;
 const modal = document.getElementById("modal");
 const modalImage = document.getElementById("modal-image");
 const closeModal =document.getElementById("close-modal");
 // FORM SUBMIT EVENT
 form.addEventListener("submit", function(event) {
-    // Prevents the page from refreshing
     event.preventDefault();
-    // Get what the user typed
     const searchTerm = input.value.trim();
     if (searchTerm !== "") {
-        searchGiphy(searchTerm);
+        currentSearchTerm = searchTerm;
+        offset = 0;
+        searchGiphy(searchTerm, true);
     }
 });
 // FETCH DATA FROM GIPHY API
@@ -31,23 +33,30 @@ form.addEventListener("submit", function(event) {
         - api_key = our API key
         - limit = number of GIFs
     */
-   async function searchGiphy(searchTerm) {
+   async function searchGiphy(searchTerm, isNewSearch = false) {
     loader.classList.remove("hidden");
     errorMessage.textContent = "";
     const url =
-        `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}&limit=12`;
+        `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}&limit=12&offset=${offset}`;
+    if (isLoading) return;
+    isLoading = true;
+    loader.classList.remove("hidden");
+    errorMessage.textContent = "";
     try {
         const response = await fetch(url);
         const data = await response.json();
         // RESET container safely
-        gifContainer.innerHTML = "";
+        if (isNewSearch) {
+            gifContainer.innerHTML = "";
+        }
         // Handle empty results FIRST
         if (data.data.length === 0) {
             errorMessage.textContent =
                 "No GIFs found. Try another search.";
             return; // stop function early
         }
-        displayGifs(data.data);
+        displayGifs(data.data, isNewSearch);
+        offset += 12;
     }
     catch (error) {
         console.log("Error fetching GIFs:", error);
@@ -56,6 +65,7 @@ form.addEventListener("submit", function(event) {
     }
     finally {
         loader.classList.add("hidden");
+        isLoading = false;
     }
 }
 // Trending Function
@@ -74,9 +84,7 @@ async function loadTrendingGifs() {
     }
 }
 // DISPLAY GIFS ON PAGE
-function displayGifs(gifs) {
-    // Clear old search results
-    gifContainer.innerHTML = "";
+function displayGifs(gifs, isNewSearch = false) {
     // Loop through each GIF
     gifs.forEach(function(gif) {
         // Create a div card
@@ -115,5 +123,21 @@ function displayGifs(gifs) {
         modal.classList.add("hidden");
         modalImage.src = "";
     }
+});
+// Infinite Scroll
+let scrollTimeout;
+window.addEventListener("scroll", function() {
+    if (isLoading) return;
+    if (currentSearchTerm === "") return;
+    clearTimeout(scrollTimeout);
+    scrollTimeout = setTimeout(() => {
+        const scrollPosition =
+            window.innerHeight + window.scrollY;
+        const pageHeight =
+            document.body.offsetHeight;
+        if (scrollPosition >= pageHeight - 500) {
+            searchGiphy(currentSearchTerm, false);
+        }
+    }, 200);
 });
 loadTrendingGifs();
